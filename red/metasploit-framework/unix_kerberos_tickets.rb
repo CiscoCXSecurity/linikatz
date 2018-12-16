4411ec4589a4cd8625b1b01168b7c444d564a853
Tim Brown <tmb@sarpedon>
2018-12-16 01:45:25 +0000

    Tidied new lines in red/metasploit-framework/*

--- red/metasploit-framework/unix_kerberos_tickets.rb
+++ red/metasploit-framework/unix_kerberos_tickets.rb
@@ -1,5 +1,5 @@
 # Copyright (c) 2015-2018, Cisco International Ltd
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #     * Redistributions of source code must retain the above copyright
@@ -10,7 +10,7 @@
 #     * Neither the name of the Cisco International Ltd nor the
 #       names of its contributors may be used to endorse or promote products
 #       derived from this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
